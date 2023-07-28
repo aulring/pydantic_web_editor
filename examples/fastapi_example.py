e819d7fe245fdb067349c9df0ef3b6ba892fe603
adam.ulring <adam.ulring@yokkal.com>
2023-07-28 14:52:17 -0500

    add schorg to demo

--- examples/fastapi_example.py
+++ examples/fastapi_example.py
@@ -5,6 +5,7 @@ from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel, Field
+from schorg.AboutPage import AboutPage
 
 from pydantic_web_editor import WebEditorConfig, copy_static_folder
 
@@ -13,21 +14,10 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 copy_static_folder(copy_path="static")
 
 
-class Hobby(str, Enum):
-    SPORTS = "sports"
-    NETFLIX = "netflix"
-
-
-class Student(BaseModel):
-    name: str = Field(description="Name of the student.")
-    classes: List[str]
-    hobby: Hobby
-    homework: Optional[bool] = None
-
 
 @app.get("/")
 def hello():
-    web_editor_config = WebEditorConfig(title="Example Pydantic Editor", model=Student, start_val=Student(name="Ruth", classes=["chemistry", "math"], hobby=Hobby("netflix")).dict())
+    web_editor_config = WebEditorConfig(title="Example Pydantic Editor demoing Schema Org's About Page", model=AboutPage, start_val={})
     return HTMLResponse(web_editor_config.html)
 
 
